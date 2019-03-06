7cae17a8ef1cf1bdae0afa5a018c9d8fcc3414b1
Zuzanna Dubanowska <2312052d@student.gla.ac.uk>
2019-03-06 15:04:51 +0000

    minor update of prev commit

--- iFood/forms.py
+++ iFood/forms.py
@@ -5,7 +5,11 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'First name'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Last name'}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'E-Mail'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':"8+ characters and please don't use your username as password"}))
     class Meta:
         model = User
         fields = ('username','first_name','last_name','email','password')
@@ -17,6 +21,9 @@ class UserForm(forms.ModelForm):
         self.helper.add_input(Submit('submit', 'Save person'))
 
 class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': '1234 Food Street'}))
+    facebook = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'https://facebook.com/your-name'}))
+    twitter = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'https://twitter.com/your-twitter-name'}))
     class Meta:
         model = UserProfile
         fields = ('address','facebook','twitter')
